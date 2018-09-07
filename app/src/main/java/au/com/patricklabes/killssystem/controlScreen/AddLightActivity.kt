16b411d40dbc0cfc5e9cb13265c717cc2f67b3af
starfish01 <patrick.labes@gmail.com>
2018-09-07 14:14:42 +1000

    lunch time work

--- app/src/main/java/au/com/patricklabes/killssystem/controlScreen/AddLightActivity.kt
+++ app/src/main/java/au/com/patricklabes/killssystem/controlScreen/AddLightActivity.kt
@@ -3,6 +3,8 @@ package au.com.patricklabes.killssystem.controlScreen
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import au.com.patricklabes.killssystem.R
+import au.com.patricklabes.killssystem.models.Light
+import au.com.patricklabes.killssystem.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -16,29 +18,53 @@ class AddLightActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_light)
 
+
         AddLightConfirmButton.setOnClickListener {
             AddALight()
         }
 
     }
 
+
+
     private fun AddALight(){
 
-        val uid = FirebaseAuth.getInstance().uid ?: return
-
-        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
-
-        ref.addListenerForSingleValueEvent(object: ValueEventListener {
-            override fun onDataChange(p0: DataSnapshot) {
-
-            }
-
-            override fun onCancelled(p0: DatabaseError) {
-
-            }
-        })
-
-
+        val newLight = Light()
 
     }
+
+    //var usersLights: MutableList<Light> = mutableListOf()
+
+
+
+//    private fun GetUsersLights(){
+//
+//        val uid = FirebaseAuth.getInstance().uid ?: return
+//
+//        val ref = FirebaseDatabase.getInstance().getReference("/lights/$uid")
+//
+//        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+//            override fun onDataChange(p0: DataSnapshot) {
+//
+////                usersLights = p0.getValue(Light::class.java)!!
+//
+//                p0.children.toList(usersLights){}
+//
+//
+////                p0.children.List(usersLights){
+////                    it.getValue<Light>(Light::class.java)
+////                }
+//
+//
+//
+//            }
+//
+//            override fun onCancelled(p0: DatabaseError) {
+//
+//            }
+//        })
+
+
+
+   // }
 }
